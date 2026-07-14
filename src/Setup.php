774e06b80fbf83fe039f4bd64b6e112e43bf007c
Robert Vogel <vogel@hallowelt.com>
2026-07-14 16:35:56 +0200

    Use $wgExtensionFunctions callback
    ... rather than MediaWikiServices hook to avoid pre-mature
    initialization of services.

--- src/Setup.php
+++ src/Setup.php
@@ -1,23 +1,19 @@
 <?php
 
-namespace MediaWiki\Extension\Appointments\Hook;
+namespace MediaWiki\Extension\Appointments;
 
 use MediaWiki\Extension\Appointments\Process\TriggerAppointmentReminders;
-use MediaWiki\Hook\MediaWikiServicesHook;
 use MediaWiki\MediaWikiServices;
 use MWStake\MediaWiki\Component\ProcessManager\ManagedProcess;
 use MWStake\MediaWiki\Component\WikiCron\WikiCronManager;
 
-class RegisterCron implements MediaWikiServicesHook {
+class Setup {
 
-	/**
-	 * @param MediaWikiServices $services
-	 * @return void
-	 */
-	public function onMediaWikiServices( $services ) {
+	public static function callback() {
 		if ( defined( 'MW_PHPUNIT_TEST' ) || defined( 'MW_QUIBBLE_CI' ) ) {
 			return;
 		}
+		$services = MediaWikiServices::getInstance();
 		/** @var WikiCronManager $cronManager */
 		$cronManager = $services->getService( 'MWStake.WikiCronManager' );
 
